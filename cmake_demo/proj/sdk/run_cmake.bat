4ad8c6830cdd91af17ccb7260165fe89667226e5
Vijay-C-Katoch <81817632+Vijay-C-Katoch@users.noreply.github.com>
2021-04-03 11:00:06 +0200

    cmake batch file modified
    cmake batch file use system cmake and version.h.in file added to create version.h in build dir

--- cmake_demo/proj/sdk/run_cmake.bat
+++ cmake_demo/proj/sdk/run_cmake.bat
@@ -1,1 +1,3 @@
-%XILINX_SDK%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
+mkdir ..\build
+chdir ..\build
+cmake.exe --trace ..\sdk > trace.txt 2>&1
--- cmake_demo/proj/sdk/run_cmake_gui.bat
+++ cmake_demo/proj/sdk/run_cmake_gui.bat
@@ -1,1 +1,1 @@
-%XILINX_SDK%\tps\win64\cmake-3.3.2\bin\cmake-gui %* 2>&1
+cmake-gui %* 2>&1
