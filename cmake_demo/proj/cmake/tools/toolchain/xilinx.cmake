59e615d45a1d46c2ae5b4e7a4b5bbcfbd9233b13
Vijay-C-Katoch <81817632+Vijay-C-Katoch@users.noreply.github.com>
2021-07-04 20:04:11 +0200

    Xilinx BSP is generated and built only once
    Xilinx BSP is generated and built only once

--- cmake_demo/proj/cmake/tools/toolchain/xilinx.cmake
+++ cmake_demo/proj/cmake/tools/toolchain/xilinx.cmake
@@ -65,13 +65,18 @@ endif()
 # use Make from Xilinx
 set(CMAKE_MAKE_PROGRAM "${XSDK_INSTALL_PATH}/gnuwin/bin/make${EXTENSION}" CACHE PATH "" FORCE)
     
-# Create bsp and sdk workspace during configuration and pass build dir argument to tcl
-#execute_process(COMMAND ${XSDK_XSCT} ${XSDK}/scripts/create_sdk_workspace.tcl ${XILINX_DEMO_BINARY_DIR}/src)
-#execute_process(COMMAND ${XSDK_XSCT} -eval source ${XSDK}/scripts/build_bsp.tcl ${XILINX_DEMO_BINARY_DIR}/src)
-
 # Create bsp and sdk workspace during build and pass build dir argument to tcl
+# NOTE: custom target is added. But we want custom target to run only once. Therefore fake commands and file 
+# generation is needed. Empty file "bsp_generated_successfully" is needed as dependency for custom target
+# in source top level CMakeLists.txt add XilinxWorkspaceTarget as dependency to any target
+
 add_custom_target(
-  XilinxWorkspaceTarget ALL
+  XilinxWorkspaceTarget
+  DEPENDS bsp_generated_successfully
+)
+add_custom_command(
+  OUTPUT bsp_generated_successfully
+  COMMAND touch bsp_generated_successfully
   COMMAND ${XSDK_XSCT} ${XSDK}/scripts/create_sdk_workspace.tcl ${XILINX_DEMO_BINARY_DIR}/src
   COMMAND ${XSDK_XSCT} -eval source ${XSDK}/scripts/build_bsp.tcl ${XILINX_DEMO_BINARY_DIR}/src
   COMMAND ${XSDK_XSCT} -eval source ${XSDK}/scripts/sdk_config.tcl ${XILINX_DEMO_BINARY_DIR}/src
