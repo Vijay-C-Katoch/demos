35ce41ae3d8a851207b7ceb07c875e08a74c45de
Vijay-C-Katoch <81817632+Vijay-C-Katoch@users.noreply.github.com>
2021-07-04 18:50:26 +0200

    Update run_cmake.bat
    CMake commands updated

--- cmake_demo/proj/sdk/run_cmake.bat
+++ cmake_demo/proj/sdk/run_cmake.bat
@@ -1,5 +1,6 @@
 mkdir ..\build
-chdir ..\build
-cmake.exe -DCMAKE_TOOLCHAIN_FILE=..\cmake\tools\toolchain\xilinx.cmake -G "Eclipse CDT4 - Unix Makefiles" --trace ..\sdk > trace.txt 2>&1
+cmake.exe -DCMAKE_TOOLCHAIN_FILE=..\cmake\tools\toolchain\xilinx.cmake -G "Eclipse CDT4 - Unix Makefiles" --trace -H..\sdk -B..\build > ..\build\trace.txt 2>&1
+cmake --build ..\build
+
 ::%XILINX_SDK%\gnuwin\bin\make
-%XILINX_SDK%\gnuwin\bin\make install
+::%XILINX_SDK%\gnuwin\bin\make install
